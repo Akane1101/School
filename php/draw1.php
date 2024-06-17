9ea931d1070610444cd7bde7a507790fc98e8395
Akane1101 <akane20031101@gmail.com>
2024-06-17 14:46:08 +0800

    Add files via upload

---
+++ php/draw1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>作品一</title>
+</head>
+<body>
+    <img src="./images/dog.jpg" alt="">
+    <p>這是我的第一個作品</p>
+</body>
+</html>
